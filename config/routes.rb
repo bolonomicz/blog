d9378d665315dce2c0824b2d3e57b60a39c85681
Ralph Bolo <ralph.bolo@gmail.com>
2013-08-09 16:40:41 -0400

    5.4 Creating the Post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Blog::Application.routes.draw do
-  get "welcome/index"
+ 
+  resources :posts
+ 
   root to: "welcome#index"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
